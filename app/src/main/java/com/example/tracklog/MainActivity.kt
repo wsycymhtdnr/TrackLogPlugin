4705884d9d0ab28d50d42531e01006f1a14b91fb
lyf <liyunfei@xiaoantech.com>
2022-04-20 20:35:29 +0800

    feat：注解处理

--- app/src/main/java/com/example/tracklog/MainActivity.kt
+++ app/src/main/java/com/example/tracklog/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.xiaoan.tracklog.annotation.FixedAttribute
 import com.xiaoan.tracklog.annotation.FixedAttributes
+import com.xiaoan.tracklog.annotation.ParameterAttribute
 import com.xiaoan.tracklog.annotation.TrackEvent
 import com.xiaoan.tracklog.beans.TrackEventBean
 import com.xiaoan.tracklog.runtime.*
@@ -23,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         findViewById<TextView>(R.id.textview).apply {
             setOnClickListener {
-                test()
+                test("dasda")
             }
         }
         TrackLog.init(object : EventSubscriber {
@@ -40,18 +41,24 @@ class MainActivity : AppCompatActivity() {
     @FixedAttributes([
         FixedAttribute(key = "key1", value = "value1", false),
         FixedAttribute(key = "key2", value = "value2", true)])
-//    @TrackEvent("test", [2,5,3])
+    @TrackEvent("test", [2,5,3])
 //    @FixedAttribute(key = "key1", value = "value1", false)
 //    @ReturnAttribute("ReturnAttribute")
-    fun test(/*@ParameterAttribute("add") add: String*/) {
-//        val filters = intArrayOf(1, 2, 3)
-//        val classTrackEvent = TrackEventBean(name, filters)
+    fun test(@ParameterAttribute("add", false) add: String) {
 //        println(classTrackEvent)
 //        val string  = "test"
 //        Log.d("lyf", string)
 //        InterningExample.example()
 //        val trackEvent = this.javaClass.getAnnotation(TrackEvent::class.java)!!
-//        TrackLogManager.sendEvent(classTrackEvent, mutableMapOf());
+
+//        val map: MutableMap<String, Any> = mutableMapOf()
+//        map["asd"] = "def"
+//        println(map)
+
+//        val filters = intArrayOf(1, 2, 3)
+//        val classTrackEvent = TrackEventBean(name, filters)
+//        val map: MutableMap<String, Any> = mutableMapOf()
+//        TrackLogManager.sendEvent(classTrackEvent, map)
     }
 
 }
--- buildSrc/src/main/kotlin/com/xiaoan/asm/TrackLogMethodVisitor.kt
+++ buildSrc/src/main/kotlin/com/xiaoan/asm/TrackLogMethodVisitor.kt
@@ -1,5 +1,6 @@
 package com.xiaoan.asm
 
+import com.sun.xml.fastinfoset.util.StringArray
 import com.xiaoan.beans.TrackEventBean
 import com.xiaoan.const.AnnotationConstants
 import com.xiaoan.const.AnnotationConstants.KEY
@@ -7,7 +8,6 @@ import com.xiaoan.const.AnnotationConstants.VALUE
 import com.xiaoan.const.AnnotationConstants.IS_SHARED
 import com.xiaoan.const.AnnotationConstants.FIXED_ATTRIBUTE
 import com.xiaoan.const.AnnotationConstants.TRACK_EVENT
-import com.xiaoan.const.AnnotationConstants.LOCAL_VARIABLE_ATTRIBUTE
 import com.xiaoan.const.AnnotationConstants.FIXED_ATTRIBUTES
 import com.xiaoan.const.AnnotationConstants.PARAMETER_ATTRIBUTE
 import com.xiaoan.const.AnnotationConstants.RETURN_ATTRIBUTE
@@ -32,12 +32,20 @@ class TrackLogMethodVisitor(
     private var trackEventName = classAttributes.name
     // class的@TraceEvent注解filters参数,解析完方法注解后加上方法注解filters
     private var filters = classAttributes.filters
-    // filters数组的标志符
-    private var filtersId :Int = 0
+    // trackLog对象在局部变量中的位置
+    private var trackLogIndex :Int = 0
+    // attributes对象在局部变量中的位置
+    private var attributesIndex :Int = 0
+    // sharedAttributes对象在局部变量中的位置
+    private var sharedAttributesIndex :Int = 0
     // 私有的参数
     private var attributes: MutableMap<String, Any> = mutableMapOf()
     // 公共的参数
-    private var shardAttributes: MutableMap<String, Any> = mutableMapOf()
+    private var sharedAttributes: MutableMap<String, Any> = mutableMapOf()
+    // @ParameterAttribute注解入参在局部变量表上的位置
+    private var parameterIndexes = intArrayOf()
+    // @ParameterAttribute注解入参name
+    private var parameterNames = StringArray()
 
     // 用于构建埋点属性的参数
     private var key = "default_key"
@@ -52,12 +60,13 @@ class TrackLogMethodVisitor(
                     super.visitEnd()
                     if (key!="default_key" && value!="default_value")
                     if (isShared) {
-                        shardAttributes[key] = value
+                        sharedAttributes[key] = value
                     } else {
                         attributes[key] = value
                     }
                 }
 
+                // 通过visitArray()来获取@FixedAttribute注解
                 override fun visitArray(name: String?): AnnotationVisitor {
                     return object: AnnotationVisitor(Opcodes.ASM7, super.visitArray(name)){
                         override fun visitAnnotation(
@@ -67,7 +76,19 @@ class TrackLogMethodVisitor(
                             return object : AnnotationVisitor(Opcodes.ASM7, super.visitAnnotation(name, descriptor)){
                                 override fun visit(name: String?, value: Any?) {
                                     super.visit(name, value)
-                                    println(name + "-----" + value)
+                                    if (descriptor == FIXED_ATTRIBUTE) {
+                                        handleFixedAttribute(name, value)
+                                    }
+                                }
+
+                                override fun visitEnd() {
+                                    super.visitEnd()
+                                    if (key!="default_key" && value!="default_value")
+                                        if (isShared) {
+                                            sharedAttributes[key] = value
+                                        } else {
+                                            attributes[key] = value
+                                        }
                                 }
                             }
                         }
@@ -75,14 +96,12 @@ class TrackLogMethodVisitor(
                 }
 
                 override fun visit(name: String?, value: Any?) {
-                    println("3" + descriptor)
                     super.visit(name, value)
                     when (descriptor) {
                         TRACK_EVENT -> handleTrackEvent(name, value)
-                        FIXED_ATTRIBUTE -> handleFixedAttribute(name, value)
+                        // FIXED_ATTRIBUTE -> handleFixedAttribute(name, value)
                         //FIXED_ATTRIBUTES -> handleFixedAttributes(name, value)
-                        LOCAL_VARIABLE_ATTRIBUTE -> {}
-                        PARAMETER_ATTRIBUTE -> {}
+                        // LOCAL_VARIABLE_ATTRIBUTE -> {}
                         RETURN_ATTRIBUTE -> {}
                     }
                 }
@@ -92,25 +111,60 @@ class TrackLogMethodVisitor(
         }
     }
 
-    // 处理@FixedAttributes注解,kotlin1.6之前不支持可重复注解
-    private fun handleFixedAttributes(name: String?, value: Any?) {
-        println("$name ---$value")
-//        (value as Array<*>).forEach {
-//            it as FixedAttribute
-//            if (it.isShared) {
-//                shardAttributes[it.key] = it.value
-//            } else {
-//                attributes[it.key] = it.value
-//            }
-//        }
+    // 访问方法入参的注解
+    override fun visitParameterAnnotation(parameter: Int, descriptor: String?, visible: Boolean): AnnotationVisitor {
+        return if (descriptor != PARAMETER_ATTRIBUTE) {
+            super.visitParameterAnnotation(parameter, descriptor, visible)
+        } else {
+
+            object: AnnotationVisitor(Opcodes.ASM7, super.visitParameterAnnotation(parameter, descriptor, visible)){
+                override fun visit(name: String?, value: Any?) {
+                    super.visit(name, value)
+                    handleParameterAttribute(name, value)
+                }
+
+                override fun visitEnd() {
+                    super.visitEnd()
+                    if (key!="default_key" && value!="default_value") {
+                        // 非静态方法第一个参数是this
+                        parameterIndexes.plus(parameter + 1)
+                        parameterNames.add(key)
+                        if (isShared) {
+                            sharedAttributes[key] = value
+                        } else {
+                            attributes[key] = value
+                        }
+                    }
+                }
+            }
+        }
     }
 
-    // 处理@TFixedAttribute注解
+    override fun visitLocalVariable(name: String?, descriptor: String?, signature: String?, start: Label?,
+                                    end: Label?, index: Int) {
+        super.visitLocalVariable(name, descriptor, signature, start, end, index)
+    }
+
+    // 处理@ParameterAttribute注解，kotlin1.6之前不支持可重复注解
+    private fun handleParameterAttribute(name: String?, value: Any?) {
+        when (name) {
+            KEY -> {
+                if (attributes.keys.contains(value.toString()) || sharedAttributes.keys.contains(value.toString())) {
+                    throw IllegalArgumentException("已经存在相同的key:---${value.toString()},请检查相关埋点注解")
+                }
+                key = value.toString()
+            }
+            IS_SHARED -> isShared = value as Boolean
+        }
+        this.value = "default_parameter_value"
+    }
+
+    // 处理@FixedAttribute注解，kotlin1.6之前不支持可重复注解
     private fun handleFixedAttribute(name: String?, value: Any?) {
         when (name) {
             KEY -> {
-                if (attributes.keys.contains(value.toString()) || shardAttributes.keys.contains(value.toString())) {
-                    throw IllegalArgumentException("已经存在相同的key:---$value.toString()")
+                if (attributes.keys.contains(value.toString()) || sharedAttributes.keys.contains(value.toString())) {
+                    throw IllegalArgumentException("已经存在相同的key:---${value.toString()},请检查相关埋点注解")
                 }
                 key = value.toString()
             }
@@ -161,45 +215,140 @@ class TrackLogMethodVisitor(
         }
 
         attributes.forEach {
-            print("attribute---$it")
+            println("attribute---$it")
         }
-        shardAttributes.forEach {
-            print("shardAttributes---$it")
+        sharedAttributes.forEach {
+            println("shardAttributes---$it")
         }
         generateTrackEventBeanInsn()
+        generateShardAttributesInsn()
+        generateAttributesInsn()
+        assignParameterAttribute()
+    }
 
+    override fun onMethodExit(opcode: Int) {
+        if (!hasTraceEvent) {
+            return
+        }
+        super.onMethodExit(opcode)
+        //generateSendEventInsn()
+    }
+
+    // 给@ParameterAttribute注解的参数赋值， visitParameterAnnotation()方法只给了一个默认值
+    private fun assignParameterAttribute() {
+        val index = newLocal(Type.INT_TYPE)
+        mv.visitLdcInsn(parameterIndexes)
+        mv.visitVarInsn(ASTORE, index)
+        mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;")
+        mv.visitVarInsn(ALOAD, index)
+        mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/Object;)V", false)
+//        parameterIndexes.forEachIndexed { index, _ ->
+//            val key = parameterNames[index]
+//            traverseAttributesMapAndModify(attributes, key, attributesIndex)
+//            traverseAttributesMapAndModify(sharedAttributes, key, sharedAttributesIndex)
+//        }
+
+    }
+
+    // 生成公共的attributes变量相关的指令
+    private fun generateShardAttributesInsn() {
+        sharedAttributesIndex = newLocal(Type.INT_TYPE)
+        mv.visitTypeInsn(NEW, "java/util/LinkedHashMap")
+        mv.visitInsn(DUP)
+        mv.visitMethodInsn(INVOKESPECIAL, "java/util/LinkedHashMap", "<init>", "()V", false);
+        mv.visitTypeInsn(CHECKCAST, "java/util/Map")
+        mv.visitVarInsn(ASTORE, sharedAttributesIndex)
+        mv.visitVarInsn(ALOAD, sharedAttributesIndex)
+        traverseAttributesMap(sharedAttributes, sharedAttributesIndex)
+        mv.visitInsn(POP)
+    }
+
+    // 生成私有的attributes相关的指令
+    private fun generateAttributesInsn() {
+        //attributes Map在局部变量表的index
+        attributesIndex = newLocal(Type.INT_TYPE)
+        mv.visitTypeInsn(NEW, "java/util/LinkedHashMap")
+        mv.visitInsn(DUP)
+        mv.visitMethodInsn(INVOKESPECIAL, "java/util/LinkedHashMap", "<init>", "()V", false);
+        mv.visitTypeInsn(CHECKCAST, "java/util/Map")
+        mv.visitVarInsn(ASTORE, attributesIndex)
+        traverseAttributesMap(attributes, attributesIndex)
+        mv.visitInsn(POP)
+    }
+
+    private fun traverseAttributesMap(map :MutableMap<String, Any>, index: Int) {
+        map.forEach {
+            mv.visitVarInsn(ALOAD, index)
+            mv.visitLdcInsn(it.key)
+            mv.visitVarInsn(ASTORE, index+1)
+            mv.visitLdcInsn(it.value)
+            mv.visitVarInsn(ASTORE, index+2)
+            mv.visitVarInsn(ALOAD, index+1)
+            mv.visitVarInsn(ALOAD, index+2)
+            mv.visitMethodInsn(INVOKEINTERFACE, "java/util/Map", "put", "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;", true);
+        }
+//        mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;")
+//        mv.visitVarInsn(ALOAD, index)
+//        mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/Object;)V", false)
+    }
+
+    private fun traverseAttributesMapAndModify(map: MutableMap<String, Any>, modifyKey: String, index: Int) {
+        map.forEach {
+            mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;")
+            mv.visitLdcInsn(modifyKey)
+            mv.visitVarInsn(ASTORE, index+1)
+            mv.visitVarInsn(ALOAD, index+1)
+            mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/Object;)V", false)
+
+            mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;")
+            mv.visitLdcInsn(it.key)
+            mv.visitVarInsn(ASTORE, index+2)
+            mv.visitVarInsn(ALOAD, index+2)
+            mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/Object;)V", false)
+
+            if (modifyKey == it.key) {
+                mv.visitVarInsn(ALOAD, index)
+                mv.visitLdcInsn(it.key)
+                mv.visitVarInsn(ASTORE, index+1)
+                mv.visitLdcInsn(it.value)
+                mv.visitVarInsn(ASTORE, index+2)
+                mv.visitVarInsn(ALOAD, index+1)
+                mv.visitVarInsn(ALOAD, index+2)
+                mv.visitMethodInsn(INVOKEINTERFACE, "java/util/Map", "put", "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;", true);
+            }
+          }
+        mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;")
+        mv.visitVarInsn(ALOAD, index)
+        mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/Object;)V", false)
+    }
+
+    // 生成发送埋点事件的指令
+    private fun generateSendEventInsn() {
+        mv.visitFieldInsn(GETSTATIC, "com/xiaoan/tracklog/runtime/TrackLogManager", "INSTANCE", "Lcom/xiaoan/tracklog/runtime/TrackLogManager;");
+        mv.visitVarInsn(ALOAD, trackLogIndex);
+        mv.visitVarInsn(ALOAD, attributesIndex);
+        mv.visitMethodInsn(INVOKEVIRTUAL, "com/xiaoan/tracklog/runtime/TrackLogManager", "sendEvent", "(Lcom/xiaoan/tracklog/beans/TrackEventBean;Ljava/util/Map;)V", false);
     }
 
     // 生成rackEventBean的相关指令
     private fun generateTrackEventBeanInsn() {
         // 创建int数组并且储存到局部变量表中
-        filtersId = newLocal(Type.INT_TYPE)
+        val filtersIndex = newLocal(Type.INT_TYPE)
         mv.visitIntInsn(SIPUSH, filterSize)
         mv.visitIntInsn(NEWARRAY, T_INT)
         initFilters()
-        mv.visitVarInsn(ASTORE, filtersId)
+        mv.visitVarInsn(ASTORE, filtersIndex)
 
         mv.visitTypeInsn(NEW, "com/xiaoan/tracklog/beans/TrackEventBean")
         mv.visitInsn(DUP)
         mv.visitLdcInsn(trackEventName)
-        mv.visitVarInsn(ALOAD, filtersId)
-        mv.visitMethodInsn(INVOKESPECIAL,
-            "com/xiaoan/tracklog/beans/TrackEventBean",
-            "<init>",
-            "(Ljava/lang/String;[I)V",
-            false)
-        //TODO object类型局部变量处理
-        val trackEventBeanId = filtersId + 1
-        mv.visitVarInsn(ASTORE, trackEventBeanId)
-
-        mv.visitInsn(ICONST_0)
+        mv.visitVarInsn(ALOAD, filtersIndex)
+        mv.visitMethodInsn(INVOKESPECIAL, "com/xiaoan/tracklog/beans/TrackEventBean", "<init>", "(Ljava/lang/String;[I)V", false)
+        trackLogIndex = newLocal(Type.INT_TYPE)
+        mv.visitVarInsn(ASTORE, trackLogIndex)
         mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;")
-        mv.visitVarInsn(ALOAD, trackEventBeanId)
-        mv.visitMethodInsn(INVOKEVIRTUAL,
-            "java/io/PrintStream",
-            "println",
-            "(Ljava/lang/Object;)V",
-            false)
+        mv.visitVarInsn(ALOAD, trackLogIndex)
+        mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/Object;)V", false)
     }
 
     // 初始化filters数组
